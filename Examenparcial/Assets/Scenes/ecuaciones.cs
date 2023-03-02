0b30249c14aebd9e446898c15a0ccbb148bfbe5b
AntonioDelgad117 <antoniodelgad117@gmail.com>
2023-03-02 12:47:03 -0600

    Examen parcial

---
+++ Examenparcial/Assets/Scenes/ecuaciones.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Euaciones : MonoBehaviour
+{
+    [SerializeField] private int temporizador;
+    public int usuario;
+    public int dadolosresultados;
+    public bool obtenerecuacion;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (obtenerecuacion)
+        {
+            int dadolosresultados;
+        }
+        if(obtenerecuacion)
+        {
+            int ecuacion;
+        }
+
+
+        
+        
+
+    }
+
+  
+}
